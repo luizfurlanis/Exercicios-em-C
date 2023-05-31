b8f5b937a5540bc81dd0f172e625547c8b6af3a9
luizfurlanis <furlanis.lu@gmail.com>
2023-05-31 15:26:11 -0300

    a

--- Ponteiros/ex8.4.c
+++ Ponteiros/ex8.4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> 
 
 void decompoeData(int diaDoAno, int ano, int *mes, int *dia);
 bool anoBissexto(int ano);
@@ -14,18 +15,18 @@ int main(){
     printf("Dia do ano: ");
     scanf("%d", &diaAno);
 
-    pritnf("Ano: ");
+    printf("Ano: ");
     scanf("%d", &ano);
 
     decompoeData(diaAno, ano, &mes, &dia);
 
-    printf("O dia %d do ano %d cai no dia %d de mes %d", dia, ano, mes, diaAno);
+    printf("O dia %d do ano %d cai no dia %d de mes %d", diaAno, ano, dia, mes);
     return 0;
 }
 
 void decompoeData(int diaDoAno, int ano, int *mes, int *dia){
 
-    int array[12] + {31,28,31,30,31,30,31,31,30,31,30,31};
+    int array[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
     if(anoBissexto){
         array[1] = 29;
@@ -34,15 +35,15 @@ void decompoeData(int diaDoAno, int ano, int *mes, int *dia){
     *dia = diaDoAno;
 
     for(int i = 0; i < 12; i++){
-        if(diaDoAno > array[i]){
+        if(*dia > array[i]){
             *mes++;
-            diaDoAno -= array[i]; 
+            *dia = *dia - array[i]; 
         }
     }
     
 }
 
-bool anoBissexto(int ano);{
+bool anoBissexto(int ano){
 
     if(ano % 4 == 0){
         return true;
