8c99e60d6b9954558e58d893b40514fecc2859cc
luizfurlanis <furlanis.lu@gmail.com>
2023-06-05 21:09:47 -0300

    a

--- Ponteiros/ex8.4.c
+++ Ponteiros/ex8.4.c
@@ -26,28 +26,16 @@ int main(){
 
 void decompoeData(int diaDoAno, int ano, int *mes, int *dia){
 
-    int array[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-    *mes = 0;
-
-    if(anoBissexto){
-        array[1] = 29;
-    }
-
-    *dia = diaDoAno;
-
-    for(int i = 0; i < 12; i++){
-        if(*dia > array[i]){
-            *mes++;
-            *dia = *dia - array[i]; 
-        }
-    }
-    
+if(diaDoAno < 32){
+    *mes = diaDoAno / 30;
+}else{
+    *mes = diaDoAno / 30 + 1;
 }
 
 bool anoBissexto(int ano){
 
-    if(ano % 4 == 0){
-        return true;
-    }
-    return false;
+if(*mes == 1 || *mes == 2){
+    *dia = diaDoAno - (*mes - 1) * 31;
+}else{
+    *dia = (diaDoAno - (*mes - 1) * 30) - 1;
 }
